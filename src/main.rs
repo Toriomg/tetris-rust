7eda72e067e8a540edee79db7caf50d948e3a734
Toriomg <hectorusuario@gmail.com>
2026-05-22 01:14:10 +0200

    siento qwue se cosas

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+struct Playfield {
+    width: u32,
+    height: u32,
+}
+
+enum Tetromino {
+    I, // 4 Cell bar
+    O, // Square
+    T,
+    S,
+    Z,
+    J,
+    L,
+}
+
+enum Cell {
+    empty,
+    taken(Tetromino),
+}
+
+struct Game{
+    board: struct Playfield,
+    playfieldMtx: [[Cell; 10]; 18]
+}
+
+impl Game {
+    fn new(){
+        board.width  = 10;
+        board.height = 10;
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
